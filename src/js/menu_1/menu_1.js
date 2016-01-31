c779ba5a7dd74aeecb3f42c6e381c47d24afdaec
Gordondalos <Gordondalos@gmail.com>
2016-02-01 00:00:55 +0600

    menu 1 анимация поиска

--- src/js/menu_1/menu_1.js
+++ src/js/menu_1/menu_1.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    $(".menu_1_main_triangle").click(function(){
-
+    $("#men").click(function(){
         if($('.menu_1_main_mega').hasClass('displays')){
             $('.menu_1_main_mega').fadeOut();
             $('.menu_1_main_mega').removeClass('displays')
@@ -8,8 +7,12 @@ $(document).ready(function() {
             $('.menu_1_main_mega').fadeIn();
             $('.menu_1_main_mega').addClass('displays')
         }
-
     });
+
+    $('#search').click(function(){
+        $('input.search').animate({width:'toggle'}, 400);
+    });
+
 
 }); // end of ready()
 
